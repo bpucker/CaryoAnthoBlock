91dfabae17b321b41950f287abb4cb9ecbc69e5d
Boas Pucker <36674497+bpucker@users.noreply.github.com>
2020-12-02 10:22:32 +0100

    Add files via upload

--- process_KIPEs_results.py
+++ process_KIPEs_results.py
@@ -1,13 +1,13 @@
 ### Boas Pucker ###
 ### bp423@cam.ac.uk ###
-### v0.1 ###
+### v0.13 ###
 
 __usage__ = """
 					python process_KIPEs_results.py
 					--in <LIST_OF_INTPUT_FOLDERS(comma separated list)>
 					--out <OUTPUT_FOLDER>
 					--genes <GENES_OF_INTEREST(comma separated list)>
-					--roi <SEMICOLON_SEP_GENES_COMMA_SEP_RESIDUES_OF_INTEREST>[False]
+					--roi <UNDERLINE_SEP_GENES_COMMA_SEP_RESIDUES_OF_INTEREST>[False]
 					"""
 
 import os, glob, sys
@@ -21,16 +21,16 @@ def get_info_per_species( inputfile ):
 	
 	infos = {}
 	with open( inputfile, "r" ) as f:
-		residues = f.readline().strip().split('\t')[1:]	#remove header
+		checked_residues = f.readline().strip().split('\t')[1:]	#remove header
 		line = f.readline()
 		while line:
 			parts = line.strip().split('\t')
 			tmp = {}
 			for idx, part in enumerate( parts[1:] ):
-				tmp.update( { residues[ idx ]: part } )
+				tmp.update( { checked_residues[ idx ]: part } )
 			infos.update( { parts[0]: tmp } )
 			line = f.readline()
-	return infos, residues
+	return infos, checked_residues
 
 
 def generate_aa_cons_fig( summary_file, fig_file, gene, residues_of_interest ):
@@ -78,6 +78,26 @@ def generate_aa_cons_fig( summary_file, fig_file, gene, residues_of_interest ):
 	plt.close( "all" )
 
 
+def load_sequences( fasta_file ):
+	"""! @brief load candidate gene IDs from file """
+	
+	sequences = {}
+	with open( fasta_file ) as f:
+		header = f.readline()[1:].strip()
+		seq = []
+		line = f.readline()
+		while line:
+			if line[0] == '>':
+					sequences.update( { header: "".join( seq ) } )
+					header = line.strip()[1:]
+					seq = []
+			else:
+				seq.append( line.strip() )
+			line = f.readline()
+		sequences.update( { header: "".join( seq ) } )	
+	return sequences
+
+
 def main( arguments ):
 	"""! @brief run everything """
 	
@@ -95,11 +115,12 @@ def main( arguments ):
 	output_folder = arguments[ arguments.index( '--out' )+1 ]
 	
 	if '--roi' in arguments:
-		all_residues_of_interest = arguments[ arguments.index( '--roi' )+1 ]
-		if ";" in all_residues_of_interest:
-			gene_parts = all_residues_of_interest.split(';')
+		tmp_all_residues_of_interest = arguments[ arguments.index( '--roi' )+1 ]
+		print tmp_all_residues_of_interest
+		if "_" in tmp_all_residues_of_interest:
+			gene_parts = tmp_all_residues_of_interest.split('_')
 		else:
-			gene_parts = [ all_residues_of_interest ]
+			gene_parts = [ tmp_all_residues_of_interest ]
 		all_residues_of_interest = []
 		for each in gene_parts:
 			if ',' in each:
@@ -108,6 +129,19 @@ def main( arguments ):
 				all_residues_of_interest.append( [ each ] )
 	else:
 		all_residues_of_interest = [ False ] * len( genes_of_interest )
+	
+	print "number of residues to check: " + str( len( all_residues_of_interest[0] ) )
+	
+	
+	if '--tolerate' in arguments:
+		tolerate = int( arguments[ arguments.index( '--tolerate' )+1 ] )
+	else:
+		tolerate = 0
+
+	if '--gaps' in arguments:
+		gaps_allowed = True
+	else:
+		gaps_allowed = False
 
 	if output_folder[-1] != '/':
 		output_folder += "/"
@@ -133,21 +167,40 @@ def main( arguments ):
 		info_output_files.update( { gene: info_output_folder + gene + ".txt" } )
 		seq_output_file_handles.update( { gene: open( seq_output_folder + gene + ".fasta", "w" ) } )
 		info_output_file_handles.update( { gene: open( info_output_folder + gene + ".txt", "w" ) } )
-
+	
+	pass_seq_counter = 0
+	header_status_per_gene = {}
+	for gene in genes_of_interest:
+		header_status_per_gene.update( { gene: True } )
 	for idx1, folder in enumerate( input_folders ):
 		sub_folders = sorted( glob.glob( folder + "*/" ) )
 		for idx2, subf in enumerate( sub_folders ):
-			for gene in genes_of_interest:
+			for g, gene in enumerate( genes_of_interest ):
 				info_file_name = subf + "conserved_residues/" + gene + "_conserved_residues.txt"
+				fasta_file = subf + "final_pep_files/" + gene + ".fasta"
 				if os.path.isfile( info_file_name ):
 					info, residues = get_info_per_species( info_file_name )
-					if idx1 == 0 and idx2 == 0:
+					seqs = load_sequences( fasta_file )
+					if header_status_per_gene[ gene ]:
 						info_output_file_handles[ gene ].write( "sequences\t" + "\t".join( residues ) + '\n' )
+						header_status_per_gene[ gene ] = False
 					for seq_info in info.keys():
 						new_line = [ seq_info ]
-						for aa in info[ seq_info ].keys():
+						for aa in residues:
 							new_line.append( info[ seq_info ][ aa ] )
 						info_output_file_handles[ gene ].write( "\t".join( new_line ) + '\n' )
+						ok_counter = 0
+						for roi in all_residues_of_interest[ g ]:
+							if info[ seq_info ][ roi ] == "True":
+								ok_counter += 1
+							elif gaps_allowed:
+								if info[ seq_info ][ roi ] == "-":
+									ok_counter += 1
+						#if "True" in new_line[1:] and len( list( set( new_line[1:]  ) ) ) == 1:
+						if ok_counter >= len( all_residues_of_interest[ g ] )-tolerate:
+							seq_output_file_handles[ gene ].write( '>' + new_line[0] + '\n' + seqs[ new_line[0] ] + '\n' )
+							pass_seq_counter += 1
+	print "number of passed genes: " + str( pass_seq_counter )
 	
 	# --- running analysis on data --- #
 	for x, gene in enumerate( genes_of_interest ):
